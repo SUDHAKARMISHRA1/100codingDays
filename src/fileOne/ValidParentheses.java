18ccccdc121f03148d681d5f074c305dc863727c
SUDHAKARMISHRA1 <mishrasudhakar5677@gmail.com>
2024-01-23 18:02:06 +0530

    ValidParenthesis

---
+++ src/fileOne/ValidParentheses.java
@@ -0,0 +1,38 @@
+package fileOne;
+
+
+public class ValidParentheses {
+
+	public static void checkParentheses(String testData) {
+
+		if(testData.length()<=1) {		
+			System.out.println("Given input string "+testData+" is not valid Parentheses");
+		}else {
+			String newData = testData.replace("{}", "").replace("()", "").replace("[]", "");
+			if(newData.length()==0) {
+				System.out.println("Given input string "+testData+" is valid Parentheses");
+			} else {
+				System.out.println("Given input string "+testData+" is not valid Parentheses");
+			}
+		}
+	}
+
+	public static void main(String[] args) {
+		String testData = "[()]][";
+		ValidParentheses.checkParentheses(testData);
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
